2045d7fb07f8e133a74e1ff507b863cea1814d53
Konstantin Alaev <alaevdev@gmail.com>
2022-06-08 19:11:35 +0300

    add new features

--- src/main/kotlin/dota/net/bigboiabudabi/BigBOiAbuDaBiApplication.kt
+++ src/main/kotlin/dota/net/bigboiabudabi/BigBOiAbuDaBiApplication.kt
@@ -2,8 +2,10 @@ package dota.net.bigboiabudabi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BigBOiAbuDaBiApplication
 
 fun main(args: Array<String>) {
